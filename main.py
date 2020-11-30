0f3f4eb4452b0a9c00f19fde78086d58c0d50a27
iJustine254 <ontita254@gmail.com>
2020-12-01 02:16:33 +0300

    Explained the code

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from pathlib import Path
 in_img = None
 out_img = None
 
-
+#   Check whether arguments were passed
+#   if yes whether:
+#   i) argument 1 is a file
+#   i) argument 2's folder exists
+#       amd if it has a png extension
+#       if the folder does not exit
+#       asks the user for permission to create it
 def check_args():
     global in_img, out_img
     if len(sys.argv) != 3:
@@ -31,22 +37,32 @@ def check_args():
             print("Sorry, I can't continue")
             exit(1)
 
-check_args()
+check_args()    #   Run the function
 
-
+#   I am import this modules here because
+#   they are only needed if the check
+#   completes successfully
+#   and also face_recognition takes time and ram
+#   no one wants to go through that when you not
+#   sure if the arguments meet the requirement
 from PIL import Image, ImageDraw
 import face_recognition as fr
 
+#   load the image
 img = fr.load_image_file(in_img)
-face_loc = fr.face_locations(img)
-face_marks = fr.face_landmarks(img)
+#   locate faces
+faces_loc = fr.face_locations(img)
 
 
 with Image.open(in_img) as im:
-    draw = ImageDraw.Draw(im)
-    cood = list(face_loc[0])
-    cood.reverse() 
-    draw.rectangle(cood, outline=155, width=7)
+    for face_loc in faces_loc:
+        draw = ImageDraw.Draw(im)
+        #   face_loc returns a set inside a list
+        #   using the set as is could not capture the face
+        cood = list(face_loc)
+        #   I sought to use a reversed list and it worked
+        cood.reverse() 
+        draw.rectangle(cood, outline=155, width=7)
     im.save(out_img, "png")
 
 
